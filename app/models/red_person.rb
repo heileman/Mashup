b3761b9a4f5239762806cee8e396b9643e1a1517
Greg <heileman@ece.unm.edu>
2010-04-29 13:58:18 -0600

    added geokit includes to red_ and blue_person

--- app/models/red_person.rb
+++ app/models/red_person.rb
@@ -1,3 +1,6 @@
+require 'geokit-rails'
+include GeoKit::Geocoders
+
 class RedPerson < ActiveRecord::Base
   has_one :location, :as => :locatable  # establish polymorphic association 
   acts_as_mappable :through => :location  # make location-based queries and find methods available
